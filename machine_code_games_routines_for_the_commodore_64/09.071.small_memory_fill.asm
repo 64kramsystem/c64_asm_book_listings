d23ea99f926f1217c7cefefdc4231193bc977c7c
Saverio Miroddi <saverio.pub2@gmail.com>
2022-01-30 20:59:43 +0100

    Add extended/optimized versions of the Small memory fill routine

--- machine_code_games_routines_for_the_commodore_64/09.071.small_memory_fill.asm
+++ machine_code_games_routines_for_the_commodore_64/09.071.small_memory_fill.asm
@@ -33,11 +33,40 @@ main:
 
 // Routine: Small memory fill //////////////////////////////////////////////////
 
+// Note that this routine does not support filling with a zero count (see below).
+//
 small_mem_fill:
 
 !loop:
           sta START_ADDR - 1, x
           dex
+          bne !loop-
+
+          rts
+
+// This version of the routine supports a zero count:
+
+small_mem_fill_unopt:
+
+          clc         // since the counter can be also 0, we can't use a bnz instead of clc+bcc; additionally
+          bcc !test+  // ...it requires X to be modified immediately before entering the routine
+
+!loop:    sta START_ADDR - 1, x
+          dex
+
+!test:    cpx #0
+          bne !loop-
+
+          rts
+
+// A nice and clean optimization can be applied, by normalizing X before performing the loop test:
+
+          inx
+          bne !test+
+
+!loop:    sta START_ADDR - 1, x
+
+!test:    dex
           bne !loop-
 
           rts
